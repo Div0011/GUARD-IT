57edefbd77c7aa43ea9bb2e1523b619ed30383c4
Div0011 <Div0011@users.noreply.github.com>
2026-04-19 21:21:32 +0000

    deploy: 70b54414c860b68ec8173553e030b0beb43970fc

---
+++ _next/static/Rf-Ace69-QAY14UR8zlL7/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/GUARD-IT//_next/:path+",
+        "destination": "/GUARD-IT/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
